e835fccc8eff882ef54333e11d4f14d44fb1200f
MCocelot <PKungpond@gmail.com>
2023-04-19 13:39:23 +0700

    first

---
+++ SAS-FrontEnd/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Navbar from './components/Navbar.vue';
+import Content from './components/Content.vue';
+import Footer from './components/Footer.vue';
+</script>
+ 
+<template>
+<div>
+  <Navbar></Navbar>
+  <Content></Content>
+  <Footer></Footer>
+</div>
+</template>
+ 
+<style scoped>
+
+</style>
---
+++ SAS-FrontEnd/src/router/index.js
@@ -0,0 +1,15 @@
+// const router=createRouter({
+//     history:createWebHistory(),
+//     routes:[
+//         {
+//             path:'/',
+//             name:'',
+//             // component:App
+//         },
+//         {
+//             path:'/',
+//             name:'',
+//             // component:App
+//         }
+//     ]
+// })
