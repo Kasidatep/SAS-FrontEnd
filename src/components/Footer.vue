ea6f16e48865b2f93c1267b75b411f8eb8cf1e7d
MCocelot <PKungpond@gmail.com>
2023-04-27 18:19:14 +0700

    27-04-66

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@
  
 <template>
     <div class="w-full h-12 bg-[#285481] mt-14 flex justify-center items-center text-white">
-        © 2023 Copyright This project was created by 3J team developer.
+        © 2023 Copyright This project was created by &nbsp;<span class="text-gray-100 cursor-pointer underline active:text-rose-400"><a href="https://github.com/TT2-3J" target="_blank">3J team developer.</a></span>
     </div>
 </template>
  
